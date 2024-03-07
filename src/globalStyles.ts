6470ea5d0e8b47d8099e34172c1c68f22246d34e
npcwalison <gwalisonls@gmail.com>
2024-03-07 16:50:56 -0300

    update

---
+++ src/globalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+  body {
+    background: #F1F1F1;
+  }
+`;
+ 
+export default GlobalStyle;
