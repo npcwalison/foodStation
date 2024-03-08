edbf30d8c0a1fb9e63bf9ab6e014febba3d22c74
npcwalison <gwalisonls@gmail.com>
2024-03-08 16:43:05 -0300

    formulario

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,8 +2,57 @@ import { Container } from './styles';
 
 
 const HomeScreen = () => {
-    return(
-        <Container className='body-container'></Container>
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        console.log('Você clicou em enviar.');
+    }
+
+    return (
+        <Container className='body-container'>
+            <form>
+                <h2>MONTE SEU HAMBURGUER</h2>
+                <div className="input-area">
+                    <p>Nome do Cliente:</p>
+                    <input type="text" placeholder='Digite aqui...' />
+                </div>
+
+                <div className="input-area">
+                    <p>Escolha o pão:</p>
+                    <select id="cars" name="cars">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </div>
+
+                <div className="input-area">
+                    <p>Escolha a carne:</p>
+                    <select id="cars" name="cars">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </div>
+
+                <div className="input-area checkbox-area">
+                    <p>Selecione os opcionais:</p>
+                    <div className='checkbox-option'>
+                        <input type="checkbox" placeholder='Digite aqui...' name='value'/>
+                        <label htmlFor="value">Queijo</label>
+                    </div>
+
+                    <div className='checkbox-option'>
+                        <input type="checkbox" placeholder='Digite aqui...' name='value'/>
+                        <label htmlFor="value">Presunto</label>
+                    </div>
+                </div>
+
+                <button onClick={handleSubmit}>Gerar Pedido!</button>
+            </form>
+        </Container>
     )
 }
 
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,4 +1,61 @@
 import styled from "styled-components";
 
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    form {
+        background-color: #ffffff;
+        width: 300px;
+        padding: 20px 10px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        h2 {
+            font-size: 20px;
+            margin-bottom: 8px;
+        }
+
+        .input-area {
+            width: 250px;
+            margin: 10px 0;
+            p {
+                font-size: 12px;
+                border-left: solid 3px #E39B1F;
+                padding: 6px;
+                margin-bottom: 6px;
+            }
+            input, select {
+                width: 100%;
+                padding: 6px 8px;
+                font-size: 12px;
+                outline: none;
+            }
+            .checkbox-option {
+                display: inline-flex;
+                margin-right: 20px;
+                input {
+                    margin-right: 4px;
+                }
+                label {
+                    font-size: 12px;
+                }
+            }
+        }
+
+        button {
+            width: 250px;
+            height: 34px;
+
+            font-size: 12px;
+            font-weight: 600;
+            color: #E39B1F;
+            background-color: #222222;
+            border: none;
+            cursor: pointer;
+        }
+    }
+`;
