43da5c16e868c0489130e2ac49ffbf15c8beda6a
npcwalison <gwalisonls@gmail.com>
2024-03-22 16:07:09 -0300

    update-form

--- src/context/getFormData.tsx
+++ src/context/getFormData.tsx
@@ -7,39 +7,44 @@ import React, {
   useState
 } from 'react';
 
+type IngredientsProps = {
+  paes: { id: number, tipo: string }[],
+  carnes: { id: number, tipo: string }[],
+  opcionais: { id: number, tipo: string }[]
+}[]
+
+type DataFormProps = {
+  ingredients: IngredientsProps;
+}
+
 
 export const DataForm = createContext<DataFormProps>({} as DataFormProps);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
+  const [ingredients, setIngredients] = useState<IngredientsProps>();
+
   useEffect(() => {
     //Resgata os pedidos
-    async function getPedidos() {
-      const req = await fetch('http://localhost:3000/burgers')
-      const data = await req.json()
-      setBurgers(data)
-
-      // Resgata os status de pedidos
-      getStatus()
+    async function getIngredients() {
+      try {
+        const response = await fetch('http://localhost:3000/ingredientes');
+        const data = await response.json();
+        setIngredients(data);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
     }
-  }, []);
 
-    //alinha os dado do array burgers com array status.
-    const handleChangeStatus = async (event: string, id: number) => {
-      const option = event;
-  
-      const dataJson = JSON.stringify({ status: option });
-  
-      await fetch(`http://localhost:3000/burgers/${id}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: dataJson
-      });
-    };
+    const data = getIngredients();
+
+    console.log(data)
+    
+  }, [ingredients]);
 
 
   return (
-    <DataForm.Provider value={{ 'value' }}>
+    <DataForm.Provider value={{ ingredients }}>
       <>{children}</>
     </DataForm.Provider>
   )
--- src/screens/register/index.tsx
+++ src/screens/register/index.tsx
@@ -1,11 +1,68 @@
+import { DataForm } from '../../context/getFormData';
 import { Container } from './styles';
+import { useContext } from 'react';
 
 
 const RegisterScreen = () => {
-    function handleSubmit(e: any) {
-        e.preventDefault();
-        console.log('Você clicou em enviar.');
+    const { ingredients } = useContext(DataForm);
+
+    const getIngredients = {
+        "paes": [
+            {
+                "id": 1,
+                "tipo": "Italiano Branco"
+            },
+            {
+                "id": 2,
+                "tipo": "3 Queijos"
+            },
+            {
+                "id": 3,
+                "tipo": "Parmesão e Orégano"
+            },
+            {
+                "id": 4,
+                "tipo": "Integral"
+            }
+        ],
+        "carnes": [
+            {
+                "id": 1,
+                "tipo": "Maminha"
+            },
+            {
+                "id": 2,
+                "tipo": "Alcatra"
+            },
+            {
+                "id": 3,
+                "tipo": "Picanha"
+            },
+            {
+                "id": 4,
+                "tipo": "Veggie burger"
+            }
+        ],
+        "opcionais": [
+            {
+                "id": 1,
+                "tipo": "Bacon"
+            },
+            {
+                "id": 2,
+                "tipo": "Cheddar"
+            },
+            {
+                "id": 3,
+                "tipo": "Salame"
+            },
+            {
+                "id": 4,
+                "tipo": "Tomate"
+            }
+        ]
     }
+
 
     return (
         <Container className='body-container'>
@@ -19,37 +76,48 @@ const RegisterScreen = () => {
                 <div className="input-area">
                     <p>Escolha o pão:</p>
                     <select id="cars" name="cars">
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="fiat">Fiat</option>
-                        <option value="audi">Audi</option>
+                        {
+                            getIngredients.paes?.map((item) => (
+                                <option key={item.id} value={item.tipo}>{item.tipo}</option>
+                            ))
+                        }
                     </select>
                 </div>
 
                 <div className="input-area">
                     <p>Escolha a carne:</p>
                     <select id="cars" name="cars">
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="fiat">Fiat</option>
-                        <option value="audi">Audi</option>
+                        {
+                            getIngredients.carnes?.map((item) => (
+                                <option key={item.id} value={item.tipo}>{item.tipo}</option>
+                            ))
+                        }
                     </select>
                 </div>
 
                 <div className="input-area checkbox-area">
                     <p>Selecione os opcionais:</p>
                     <div className='checkbox-option'>
-                        <input type="checkbox" placeholder='Digite aqui...' name='value'/>
+                        <input type="checkbox" placeholder='Digite aqui...' name='value' />
                         <label htmlFor="value">Queijo</label>
                     </div>
 
                     <div className='checkbox-option'>
-                        <input type="checkbox" placeholder='Digite aqui...' name='value'/>
+                        <input type="checkbox" placeholder='Digite aqui...' name='value' />
                         <label htmlFor="value">Presunto</label>
                     </div>
+
+                    {
+                        getIngredients.opcionais?.map((item) => (
+                            <div className='checkbox-option' key={item.id}>
+                                <input type="checkbox" value={item.tipo} name='value' />
+                                <label htmlFor="value">{item.tipo}</label>
+                            </div>
+                        ))
+                    }
                 </div>
 
-                <button onClick={handleSubmit}>Gerar Pedido!</button>
+                <button>Gerar Pedido!</button>
             </form>
         </Container>
     )
